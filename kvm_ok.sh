0145199e5930f34ea26c50b30a7cd7193f9bd312
Mike Day <ncmike@ncultra.org>
2014-01-15 08:33:15 -0500

    brief script to check memory and vmx | svm

---
+++ kvm_ok.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash   
+# path ok for fedora - TODO update for other distros
+
+# print the amount of memory
+echo "Your computer should have 2 GB of RAM or more"
+echo 
+dmesg | grep Memory:
+echo
+read -s  -n1 -p"(Press any key to proceed...)"
+echo
+echo "The following command will verify that your processor has virtualization enabled."
+echo "If there is no output, you need to enter Setup and enable virtualization in BIOS."
+echo
+read -s  -n1 -p"(Press any key to proceed...)"
+echo
+grep -E "(vmx|svm)" --color=always /proc/cpuinfo
