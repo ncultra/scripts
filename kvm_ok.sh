a8d14cbb68595c4af256d7812eb28257d3f1a49b
Mike Day <ncmike@ncultra.org>
2014-01-15 14:19:04 -0500

    better line spacing

--- kvm_ok.sh
+++ kvm_ok.sh
@@ -2,16 +2,11 @@
 # path ok for fedora - TODO update for other distros
 
 # print the amount of memory
-echo "Your computer should have 2 GB of RAM or more"
-echo 
+echo "Your computer should have 2 GB of RAM or more for this course."
+echo -e "\n"
 dmesg | grep Memory:
 echo
 read -s  -n1 -p"(Press any key to proceed...)"
-echo
-echo "The following command will verify that your processor has virtualization enabled."
-echo "If there is no output, you need to enter Setup and enable virtualization in BIOS."
-echo "It will print the number of processors that ARE enabled for virtualization..."
-echo
-read -s  -n1 -p"(Press any key to proceed...)"
-echo
-grep -E "(vmx|svm)" --color=always /proc/cpuinfo | wc -l
+echo -e "\n"
+PROCS=$(grep -E "(vmx|svm)" --color=always /proc/cpuinfo | wc -l)
+echo "$PROCS  processors are enabled for virtualization"
